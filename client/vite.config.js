63c3ab6c3cce8ff00537603ca896a0d11ee750b2
Jeremiah Haastrup <107923171+thetalesofj@users.noreply.github.com>
2023-04-08 17:29:27 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://localhost:4173',
+  base: 'gericht-challenge.vercel.app',
   esbuild: {
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
