0b9a4ea75d93cce67d4b0936a3766c3ec4eb8605
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2023-04-10 17:15:53 +0100

    v1.0.3

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
       <div>
         <video className='absolute top-0 left-0 min-w-full min-h-full object-cover' autoPlay={true} muted loop playsInline={true} ><source src='https://res.cloudinary.com/drecxkgks/video/upload/v1681127550/GERICHT%20-%20CHALLENGE%20FOLDER/bg-video_nzthmd.mp4'/></video>
       </div>
-      <div className='flex items-center justify-center sm:pt-[23rem] tablet:pt-[25rem] desktop:pt-[27rem]'>
+      <div className='flex items-center justify-center landscape:sm:pt-32 sm:pt-[23rem] landscape:mobile:pt-40 tablet:pt-[25rem] landscape:desktop:pt-[20rem] desktop:pt-[27rem]'>
         <p className='absolute text-white font-titles text-center sm:text-4xl mobile:text-5xl tablet:text-6xl desktop:text-8xl'>GERÍCHT</p> 
       </div>
     </div>
